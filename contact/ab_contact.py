8b2901584913395ae4bdc56db3c06e276d07871f
Igor Matveev <veevtamrogi@yandex.ru>
2024-05-11 16:47:03 +0300

    change classes and gitignore file

--- contact/ab_contact.py
+++ contact/ab_contact.py
@@ -1,31 +1,75 @@
-import abc
+"""
+словарь отсортирован в алфавитном порядке
+бинарный поиск
+"""
+
+from abc import ABC, abstractmethod
+from datetime import datetime
 
 
-class Contact(abc.ABC):
+class Contact(ABC):
 
     def __init__(self, **kwargs) -> None:
-        pass
+        self.id_contact = id_contact
+        self.email = kwargs['email']
+        self.address = kwargs['address']
+        self.created_at = datetime.now()
 
-    @abc.abstractmethod
+    @abstractmethod
     def create_contact(self) -> dict:
-        ...
+        new_contact: dict = {
+            'id_contact': self.id_contact,
+            'email': self.email,
+            'address': self.address,
+            'created_at': self.created_at
+        }
+        return new_contact
 
-    @abc.abstractmethod
-    def change_contact(self) -> dict:
-        ...
+    @abstractmethod
+    def change_contact(self, contact: dict, attr: str, new_attr: str) -> None:
+        contact[attr] = new_attr
+
+    @abstractmethod
+    def delete_contact(self, database: list, contact: dict) -> None:
+        database.remove(contact)
+
+    def __str__(self): pass
 
 
 class PersonContactMixin:
-    ...
 
-
-class PersonContact(Contact):
-
-    def __init__(self, **kwargs):
-        super().__init__()
+    def __init__(self, **kwargs) -> None:
+        self.first_name = kwargs['first_name']
+        self.last_name = kwargs['last_name']
+        self.full_name = self.last_name + ' ' + self.first_name
 
     def create_contact(self) -> dict:
-        ...
+        new_contact: dict = {
+            'first_name': self.first_name,
+            'second_name': self.last_name,
+            'full_name': self.full_name
+        }
+        return new_contact
 
-    def change_contact(self) -> dict:
-        ...
+    def change_contact(self): pass
+
+    def delete_contact(self): pass
+
+    def __str__(self): pass
+
+
+class PersonContact(Contact, PersonContactMixin):
+
+    def __init__(self) -> None:
+        super().__init__()
+        PersonContactMixin.__init__(self)
+
+    def create_contact(self) -> dict:
+        new_contact = super().create_contact() | PersonContactMixin.create_contact(self)
+        return new_contact
+
+    def change_contact(self) -> dict: pass
+
+    def delete_contact(self) -> dict: pass
+
+    def __str__(self): pass
--- file_handler/ab_file_handler.py
+++ file_handler/ab_file_handler.py
@@ -1,32 +1,14 @@
-import abc
 import json
 
 
-class FileHandler(abc.ABC):
-
-    def __init__(self, filename: str, ext: str, ) -> None:
-        self.filename = filename
-        self.ext = ext
-
-    @abc.abstractmethod
-    def read_file(self, filename: str):
+class JsonFileHandler:
+    
+    @staticmethod
+    def read_file(filename: str) -> list:
         with open(filename, mode='r', encoding='UTF-8') as file:
-            return file
-
-    @abc.abstractmethod
-    def change_file(self, filename: str, data):
-        pass
-
-
-class JsonFileHandler(FileHandler):
-
-    def __init__(self, filename: str, ext: str = 'json') -> None:
-        super().__init__(filename, ext)
-
-    def read_file(self, filename: str) -> list:
-        with super().read_file(filename) as file:
             return json.load(file)
 
-    def change_file(self, filename: str, data):
+    @staticmethod
+    def write_file(filename: str, data):
         with open(filename, 'w', encoding='UTF-8') as file:
             json.dump(data, file, indent=4, ensure_ascii=False)
